69ff955faacb99ae7e6872f6fbe597f05516ff0c
carlos-augusto-carneiro <devcarlosaugusto@outlook.com>
2026-01-22 13:47:47 -0300

    Feat: Adicionado page automatos

---
+++ src/app/pages/automatos/automatos.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Card } from '../../component/card/card';
+
+@Component({
+  selector: 'app-automatos',
+  imports: [Card],
+  templateUrl: './automatos.html',
+  styleUrl: './automatos.css',
+})
+export class Automatos {
+
+}
